391f7acb232c40ba8d094722eb1e8e12e6b9aef5
ttarnok <128530337+ttarnok@users.noreply.github.com>
2024-04-12 09:37:24 +0200

    Implement bad request handling

--- cmd/api/instruments.go
+++ cmd/api/instruments.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 	"strconv"
 
@@ -45,7 +44,7 @@ func (app *application) createInstrumentHandler(w http.ResponseWriter, r *http.R
 		FamousOwners    []string `json:"famous_owners"`
 	}
 
-	err := json.NewDecoder(r.Body).Decode(&input)
+	err := app.readJSON(r, &input)
 	if err != nil {
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 		return

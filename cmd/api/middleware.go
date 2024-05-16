3d237fb77bb0c2308c2f1e3c10cbe201c2487562
ttarnok <128530337+ttarnok@users.noreply.github.com>
2024-05-16 15:49:44 +0200

    Implement CORS middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -131,3 +131,22 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 
 	})
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+
+			w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}

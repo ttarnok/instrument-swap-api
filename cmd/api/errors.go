c43a4d7de60f1cc22224d97ecef727e0134c18cd
ttarnok <128530337+ttarnok@users.noreply.github.com>
2024-06-27 14:40:33 +0200

    Refactor comments in errors.go

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-// Helper for logging errors in handlers.
+// logError logs and error message, besides request related informations.
 func (app *application) logError(r *http.Request, err error) {
 	var (
 		method = r.Method
@@ -13,7 +13,7 @@ func (app *application) logError(r *http.Request, err error) {
 	app.logger.Error(err.Error(), "method", method, "uri", uri)
 }
 
-// Helper for sending JSON formatted error responses.
+// errorResponse sends well formatted error response to the client.
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
 
 	env := envelope{"error": message}
@@ -25,7 +25,7 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 	}
 }
 
-// Helper to respond internal server error to the client.
+// serverErrorLogResponse sends InternalServerError response to the client and logs the error that caused it.
 func (app *application) serverErrorLogResponse(w http.ResponseWriter, r *http.Request, err error) {
 
 	app.logError(r, err)
@@ -35,40 +35,50 @@ func (app *application) serverErrorLogResponse(w http.ResponseWriter, r *http.Re
 
 }
 
+// notFoundResponse send NotFound response to the client.
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 	message := "the requested resource could not be found"
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
-// Helper to respond json validation errors.
+// failedValidationResponse send UnprocessableEntity error to the client.
+// Also sends a fields/errors map that contains the failed entries during user input validation.
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
 
+// badRequestResponse sends BadRequest response to the client and an error that contains the problem.
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
 
+// editConflictResponse sends ConflictResponse response to the client.
 func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
 	message := "unable to update the record due to an edit conflict, please try again"
 	app.errorResponse(w, r, http.StatusConflict, message)
 }
 
+// swappedInstrumentResponse...
 func (app *application) swappedInstrumentResponse(w http.ResponseWriter, r *http.Request) {
 	message := "can not perform the operation on a swapped instrument"
 	app.errorResponse(w, r, http.StatusConflict, message)
 }
 
+// rateLimitExcededResponse sends TooManyRequests to the client.
 func (app *application) rateLimitExcededResponse(w http.ResponseWriter, r *http.Request) {
 	message := "rate limit exceeded"
 	app.errorResponse(w, r, http.StatusTooManyRequests, message)
 }
 
+// invalidCredentialsResponse sends Unauthorized response  to the client.
+// Indicates invalid authentication credentials.
 func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *http.Request) {
 	message := "invalid authentication credentials"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
 
+// invalidAuthenticationTokenResponse sends Unauthorized response to the client.
+// Indicates invalid or missing authentication token.
 func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("WWW-Authenticate", "Bearer")
 
@@ -76,11 +86,15 @@ func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
 
+// authenticationRequiredResponse sends Unauthorized response to the client.
+// Indicates that the user is trying to request a resource that requires an authenticated user.
 func (app *application) authenticationRequiredResponse(w http.ResponseWriter, r *http.Request) {
 	message := "you must be authenticated to access this resource"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
 
+// inactiveAccountResponse Forbidden response to the client.
+// Indicates that the user that is initiating the request is not activated yet.
 func (app *application) inactiveAccountResponse(w http.ResponseWriter, r *http.Request) {
 	message := "your user account must be activated to access this resource"
 	app.errorResponse(w, r, http.StatusForbidden, message)

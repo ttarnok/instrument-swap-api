24c1f6e9427b2e64aa40bed2cffcee79aa15b66a
ttarnok <128530337+ttarnok@users.noreply.github.com>
2024-06-27 15:20:55 +0200

    Implement unit test for serverErrorLogResponse

--- cmd/api/errors_test.go
+++ cmd/api/errors_test.go
@@ -71,7 +71,7 @@ func TestLogError(t *testing.T) {
 // TestErrorResponse is testing the happy path of errorResponse.
 func TestErrorResponse(t *testing.T) {
 
-	expextedErrorMessage := "test error message"
+	expextedErrorMsg := "test error message"
 	expectedContentType := "application/json"
 
 	app := &application{}
@@ -82,7 +82,7 @@ func TestErrorResponse(t *testing.T) {
 	}
 	w := httptest.NewRecorder()
 
-	app.errorResponse(w, r, http.StatusInternalServerError, expextedErrorMessage)
+	app.errorResponse(w, r, http.StatusInternalServerError, expextedErrorMsg)
 
 	resp := w.Result()
 	body, _ := io.ReadAll(resp.Body)
@@ -103,8 +103,90 @@ func TestErrorResponse(t *testing.T) {
 		t.Errorf(`expected content type "%s", got "%s"`, expectedContentType, resp.Header.Get("Content-Type"))
 	}
 
-	if jRes.Error != expextedErrorMessage {
-		t.Errorf(`expected errors message "%s", got "%s"`, expextedErrorMessage, jRes.Error)
+	if jRes.Error != expextedErrorMsg {
+		t.Errorf(`expected errors message "%s", got "%s"`, expextedErrorMsg, jRes.Error)
+	}
+
+}
+
+// TestServerErrorLogResponse tests the happy path of serverErrorLogResponse.
+func TestServerErrorLogResponse(t *testing.T) {
+	testErr := errors.New("test error")
+
+	expectedLevel := "ERROR"
+	expextedMsg := testErr.Error()
+	exPectedMethod := "GET"
+	expectedURI := "/path"
+
+	expextedErrorMsg := "the server encountered a problem and could not process your request"
+	expectedContentType := "application/json"
+
+	url := fmt.Sprintf("https://www.example.com%s", expectedURI)
+
+	buf := &bytes.Buffer{}
+
+	logger := slog.New(slog.NewJSONHandler(buf, &slog.HandlerOptions{}))
+	app := &application{logger: logger}
+
+	r, err := http.NewRequest(exPectedMethod, url, nil)
+	if err != nil {
+		t.Fatal("cannot set up request for testing")
+	}
+
+	w := httptest.NewRecorder()
+
+	app.serverErrorLogResponse(w, r, testErr)
+
+	var jRes struct {
+		Time   time.Time `json:"time"`
+		Level  string    `json:"level"`
+		Msg    string    `json:"msg"`
+		Method string    `json:"method"`
+		URI    string    `json:"uri"`
+	}
+
+	err = json.Unmarshal(buf.Bytes(), &jRes)
+	if err != nil {
+		t.Fatal("cannot marshall data for testing")
+	}
+
+	if jRes.Level != expectedLevel {
+		t.Errorf(`expected "%s", got "%s"`, expectedLevel, jRes.Level)
+	}
+
+	if jRes.Msg != expextedMsg {
+		t.Errorf(`expected "%s", got "%s"`, expextedMsg, jRes.Msg)
+	}
+
+	if jRes.Method != exPectedMethod {
+		t.Errorf(`expected "%s", got "%s"`, exPectedMethod, jRes.Method)
+	}
+
+	if jRes.URI != expectedURI {
+		t.Errorf(`expected "%s", got "%s"`, expectedURI, jRes.URI)
+	}
+
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+
+	var jErrRes struct {
+		Error string `json:"error"`
+	}
+	err = json.Unmarshal(body, &jErrRes)
+	if err != nil {
+		t.Fatal("cannot unmarshal json for text")
+	}
+
+	if resp.StatusCode != http.StatusInternalServerError {
+		t.Errorf(`expected status code %d, got %d`, http.StatusInternalServerError, resp.StatusCode)
+	}
+
+	if resp.Header.Get("Content-Type") != expectedContentType {
+		t.Errorf(`expected content type "%s", got "%s"`, expectedContentType, resp.Header.Get("Content-Type"))
+	}
+
+	if jErrRes.Error != expextedErrorMsg {
+		t.Errorf(`expected errors message "%s", got "%s"`, expextedErrorMsg, jErrRes.Error)
 	}
 
 }

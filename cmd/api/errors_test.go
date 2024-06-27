bb8686744cd5e62a03d5b6ee56596a14cde2716c
ttarnok <128530337+ttarnok@users.noreply.github.com>
2024-06-27 16:59:49 +0200

    Implement unit test for invalidAuthenticationTokenResponse

--- cmd/api/errors_test.go
+++ cmd/api/errors_test.go
@@ -430,3 +430,46 @@ func TestInvalidCredentialsResponse(t *testing.T) {
 		t.Errorf(`expected status code %d, goit %d`, expectedStatusCode, resp.StatusCode)
 	}
 }
+
+// TestInvalidAuthenticationTokenResponse tests the happy path for invalidAuthenticationTokenResponse.
+func TestInvalidAuthenticationTokenResponse(t *testing.T) {
+	expectesErrorMsg := "invalid or missing authentication token"
+	expectedStatusCode := http.StatusUnauthorized
+	expectedWWWAuthenticate := "Bearer"
+
+	app := &application{}
+
+	url := "https://www.example.com/path"
+
+	r, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatal("cannot set up request for testing")
+	}
+
+	w := httptest.NewRecorder()
+
+	app.invalidAuthenticationTokenResponse(w, r)
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+
+	var jErrRes struct {
+		Error string `json:"error"`
+	}
+
+	err = json.Unmarshal(body, &jErrRes)
+	if err != nil {
+		t.Fatal("cannot unmarshal json for text")
+	}
+
+	if jErrRes.Error != expectesErrorMsg {
+		t.Errorf(`expected response body "%#v", got "%#v"`, expectesErrorMsg, jErrRes.Error)
+	}
+
+	if resp.StatusCode != expectedStatusCode {
+		t.Errorf(`expected status code %d, goit %d`, expectedStatusCode, resp.StatusCode)
+	}
+
+	if resp.Header.Get("WWW-Authenticate") != expectedWWWAuthenticate {
+		t.Errorf(`expected WWW-Authenticate header "%s", got "%s"`, expectedWWWAuthenticate, resp.Header.Get("WWW-Authenticate"))
+	}
+}

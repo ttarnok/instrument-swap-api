2647f79b08ca8a0d3f279c5ced964237c46980fb
ttarnok <128530337+ttarnok@users.noreply.github.com>
2024-07-12 14:12:00 +0200

    Implement unit tests for enableCORS middleware

--- cmd/api/middleware_test.go
+++ cmd/api/middleware_test.go
@@ -192,3 +192,96 @@ func TestRateLimit(t *testing.T) {
 	}
 
 }
+
+// TestEnableCORS implements unit tests for enableCORS middleware.
+func TestEnableCORS(t *testing.T) {
+
+	type testCase struct {
+		name                       string
+		requestMethod              string
+		sendACRMHeader             bool
+		expectedStatusCode         int
+		shouldContainOptionsHeader bool
+	}
+
+	testCases := []testCase{
+		{
+			name:                       "with options request, with ACMR",
+			requestMethod:              http.MethodOptions,
+			sendACRMHeader:             true,
+			expectedStatusCode:         http.StatusOK,
+			shouldContainOptionsHeader: true,
+		},
+		{
+			name:                       "without options request, without ACMR",
+			requestMethod:              http.MethodGet,
+			sendACRMHeader:             false,
+			expectedStatusCode:         http.StatusOK,
+			shouldContainOptionsHeader: false,
+		},
+		{
+			name:                       "without options request, with ACMR",
+			requestMethod:              http.MethodGet,
+			sendACRMHeader:             true,
+			expectedStatusCode:         http.StatusOK,
+			shouldContainOptionsHeader: false,
+		},
+		{
+			name:                       "with options request, without ACMR",
+			requestMethod:              http.MethodOptions,
+			sendACRMHeader:             false,
+			expectedStatusCode:         http.StatusOK,
+			shouldContainOptionsHeader: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			app := &application{}
+
+			req, err := http.NewRequest(tc.requestMethod, "/", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if tc.sendACRMHeader {
+				req.Header.Set("Access-Control-Request-Method", "GET")
+			}
+
+			next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_, err := w.Write([]byte("OK"))
+				if err != nil {
+					t.Fatal(err)
+				}
+			})
+
+			rr := httptest.NewRecorder()
+
+			app.enableCORS(next).ServeHTTP(rr, req)
+			recRes := rr.Result()
+
+			if tc.expectedStatusCode != recRes.StatusCode {
+				t.Errorf(`expected status code %d, got %d`, tc.expectedStatusCode, recRes.StatusCode)
+			}
+
+			if recRes.Header.Get("Vary") != "Access-Control-Request-Method" {
+				t.Errorf(`response shoud contain "Vary" header with the value of "Access-Control-Request-Method", got value: "%s"`, recRes.Header.Get("Vary"))
+			}
+
+			if recRes.Header.Get("Access-Control-Allow-Origin") != "*" {
+				t.Errorf(`response shoud contain "Access-Control-Allow-Origin" header with the value of "*", got value: "%s"`, recRes.Header.Get("Access-Control-Allow-Origin"))
+			}
+
+			if tc.shouldContainOptionsHeader {
+				if recRes.Header.Get("Access-Control-Allow-Methods") != "OPTIONS, PUT, PATCH, DELETE" {
+					t.Errorf(`response shoud contain "Access-Control-Allow-Methods" header with the value of "OPTIONS, PUT, PATCH, DELETE" got value: "%s"`, recRes.Header.Get("Access-Control-Allow-Methods"))
+				}
+				if recRes.Header.Get("Access-Control-Allow-Headers") != "Authorization, Content-Type" {
+					t.Errorf(`response shoud contain "Access-Control-Allow-Headers" header with the value of "Authorization, Content-Type" got value: "%s"`, recRes.Header.Get("Access-Control-Allow-Headers"))
+				}
+			}
+
+		})
+	}
+
+}

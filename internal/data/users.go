b82706f5ecd62fa4e35dc653ad7e219f73d51a50
ttarnok <128530337+ttarnok@users.noreply.github.com>
2024-08-06 10:38:39 +0200

    Reformat user model code comments

--- internal/data/users.go
+++ internal/data/users.go
@@ -19,6 +19,7 @@ var (
 // AnonymousUser represents an unauthenticated user.
 var AnonymousUser = &User{}
 
+// User struct represents an user.
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -29,15 +30,18 @@ type User struct {
 	Version   int       `json:"-"`
 }
 
+// IsAnonymous returns true if the given user is AnonymousUser.
 func (u *User) IsAnonymous() bool {
 	return u == AnonymousUser
 }
 
+// password is and internal type to help dealing with passwords.
 type password struct {
 	plaintext *string
 	hash      []byte
 }
 
+// Set sets and encrypts the given password string.
 func (p *password) Set(plaintextPassword string) error {
 	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
 	if err != nil {
@@ -50,6 +54,7 @@ func (p *password) Set(plaintextPassword string) error {
 	return nil
 }
 
+// Matches compares the given password string with the stored encrypted password.
 func (p *password) Matches(plaintextPassword string) (bool, error) {
 	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
 	if err != nil {
@@ -64,17 +69,20 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	return true, nil
 }
 
+// ValidateEmail validates the given email address.
 func ValidateEmail(v *validator.Validator, email string) {
 	v.Check(email != "", "email", "must be provided")
 	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email addrerss")
 }
 
+// ValidatePasswordPlaintext validates the format of the given password.
 func ValidatePasswordPlaintext(v *validator.Validator, password string) {
 	v.Check(password != "", "password", "must be provided")
 	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
 	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
 }
 
+// ValidateUser validates the fields of the given user.
 func ValidateUser(v *validator.Validator, user *User) {
 	v.Check(user.Name != "", "name", "must be provided")
 	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
@@ -90,10 +98,13 @@ func ValidateUser(v *validator.Validator, user *User) {
 	}
 }
 
+// UserModel represents the user model, that stores users in a database.
 type UserModel struct {
 	DB *sql.DB
 }
 
+// Insert inserts the given user into the database.
+// Returns ErrDuplicateEmail if the given email is already stored.
 func (m *UserModel) Insert(user *User) error {
 	query := `
 		INSERT INTO users (name, email, password_hash, activated)
@@ -118,6 +129,7 @@ func (m *UserModel) Insert(user *User) error {
 	return nil
 }
 
+// GetAll retrieves all users from the database.
 func (m *UserModel) GetAll() (users []*User, err error) {
 	query := `
 	SELECT id, created_at, name, email, password_hash, activated, version
@@ -170,6 +182,8 @@ func (m *UserModel) GetAll() (users []*User, err error) {
 
 }
 
+// GetByEmail retrieves the user from the database with the given email.
+// Returns ErrRecordNotFound if the user is not found.
 func (m *UserModel) GetByEmail(email string) (*User, error) {
 	query := `
 		SELECT id, created_at, name, email, password_hash, activated, version
@@ -204,6 +218,8 @@ func (m *UserModel) GetByEmail(email string) (*User, error) {
 	return &user, nil
 }
 
+// GetByID retrieves the user from the database with the given user id.
+// Returns ErrRecordNotFound if the user is not found.
 func (m *UserModel) GetByID(id int64) (*User, error) {
 	query := `
 		SELECT id, created_at, name, email, password_hash, activated, version
@@ -238,6 +254,9 @@ func (m *UserModel) GetByID(id int64) (*User, error) {
 	return &user, nil
 }
 
+// Update updates the given user in the databse.
+// Returns ErrDuplicateEmail if the given email is already stored in the database.
+// Returns ErrEditConflict in case of conflicting update.
 func (m *UserModel) Update(user *User) error {
 	query := `
 		UPDATE users
@@ -274,6 +293,8 @@ func (m *UserModel) Update(user *User) error {
 	return nil
 }
 
+// Delete deletes the user from the database with the given user id.
+// Returns ErrRecordNotFound if the given user is not found.
 func (m *UserModel) Delete(id int64) error {
 
 	if id < 0 {

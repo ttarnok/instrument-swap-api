51bb9a87e40b6aacd3c5c3fbb57e57109fc9bde6
ttarnok <128530337+ttarnok@users.noreply.github.com>
2024-08-11 11:36:57 +0200

    Refactor init.sql

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,7 +1,2 @@
 -- init.sql
--- CREATE DATABASE instrumentswap;
--- \c instrumentswap
--- CREATE ROLE instrumentswap WITH LOGIN PASSWORD 's3cr3t';
 CREATE EXTENSION IF NOT EXISTS citext;
-
--- ALTER DATABASE instrumentswap OWNER TO instrumentswap;
